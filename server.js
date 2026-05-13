46fda64075bb36ede399ca4f203b3a177e48529d
muhrizky645-png <muhrizky645@gmail.com>
2026-05-13 15:42:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const QRCode = require("qrcode");
 
 const makeDynamicQRIS = require("./qris");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
 
     const qris =
 "00020101021126690021ID.CO.BANKMANDIRI.WWW01189360000801662700160211716627001640303UMI51440014ID.CO.QRIS.WWW0215ID10243516740060303UMI5204274153033605802ID5915Pijat Mas Jamal6012Sleman (Kab)61055551362070703A016304194B";
 
     const result = makeDynamicQRIS(qris, 15000);
 
-    res.send({
-        status: true,
-        amount: 15000,
-        qris: result
-    });
+    const qrImage = await QRCode.toDataURL(result);
+
+    res.send(`
+        <h1>QRIS Dynamic</h1>
+        <p>Nominal: Rp15.000</p>
+        <img src="${qrImage}" />
+    `);
 
 });
 
