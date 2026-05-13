9c1e18b15452e30dee034fe515fcfd8340ad77b5
muhrizky645-png <muhrizky645@gmail.com>
2026-05-13 16:07:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const makeDynamicQRIS = require("./qris");
 
 const app = express();
 
-app.get("/", async (req, res) => {
-
-    const qris =
+const qris =
 "00020101021126690021ID.CO.BANKMANDIRI.WWW01189360000801662700160211716627001640303UMI51440014ID.CO.QRIS.WWW0215ID10243516740060303UMI5204274153033605802ID5915Pijat Mas Jamal6012Sleman (Kab)61055551362070703A016304194B";
 
-    const result = makeDynamicQRIS(qris, 15000);
+app.get("/", (req, res) => {
+    res.send("QRIS Gateway Running");
+});
+
+app.get("/pay", async (req, res) => {
+
+    const amount = req.query.amount || 1000;
+
+    const result = makeDynamicQRIS(qris, amount);
 
     const qrImage = await QRCode.toDataURL(result);
 
     res.send(`
         <h1>QRIS Dynamic</h1>
-        <p>Nominal: Rp15.000</p>
+        <p>Nominal: Rp${Number(amount).toLocaleString("id-ID")}</p>
         <img src="${qrImage}" />
     `);
 
