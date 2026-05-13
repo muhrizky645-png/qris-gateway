cfe8104963e0296a066aa611c7c253c10191b1b2
muhrizky645-png <muhrizky645@gmail.com>
2026-05-13 15:37:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const toCRC16 = require("./crc16");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("QRIS Gateway Running");
+
+    const crc = toCRC16("TEST");
+
+    res.send({
+        message: "QRIS Gateway Running",
+        crc: crc
+    });
+
 });
 
 app.listen(3000, () => {
